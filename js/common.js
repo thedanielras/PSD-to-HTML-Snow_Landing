9d5732cde80713a42154a377f824a38863626968
root <root@DESKTOP-J5IFQ89.localdomain>
2018-09-13 22:11:34 +0300

    Improvemets

--- js/common.js
+++ js/common.js
@@ -1,16 +1,3 @@
-$(function() {
-	$(".hamburger").click(function() {
-		$(".hamburger").toggleClass(" is-active");
-		$(".hamburger-inner").toggleClass(" is-black");
-		$(".main-nav").toggleClass(" show");
-	});
-	$(".anchor").click(function(){
-		$(".main-nav").toggleClass(" show");
-		$(".hamburger-inner").toggleClass(" is-black");
-		$(".hamburger").toggleClass(" is-active");
-	});
-});
-
 	$(document).ready(function(){
 		$(".owl-carousel").owlCarousel({
 			loop: true,
@@ -21,46 +8,71 @@ $(function() {
 			dotsContainer: ".owl-buttons"
 		});
 
-	$(".clients-carousel").owlCarousel({
-		loop: true,
-		items: 5,
-		nav: false,
-		dots: false,
-		center: false,
-		autoWidth: true,
-		margin: 0
-	});
-	
-	$(".blog-section__blogpost-carousel").owlCarousel({
-		loop: false,
-		items: 3,
-		dots: true,
-		autoplay: true,
-		autoplayHoverPause: true,
-		center: false,
-		autoWidth: false,
-		dotsContainer: ".blogpost-carousel_owl-buttons"
-	});
+		$(".hamburger, .anchor").click(function() {
+			$(".hamburger").toggleClass(" is-active");
+			$("body").toggleClass(" noflow");
+			$(".hamburger-inner").toggleClass(" is-black");
+			$(".main-nav").toggleClass(" show");
+		});
 
-	$("a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+		$(".clients-carousel").owlCarousel({
+			loop: true,
+			items: 5,
+			nav: false,
+			dots: false,
+			center: false,
+			autoWidth: true,
+			margin: 0,
+			center: true,
+			responsive: {
+				1150: {
+					items: 1
+				}
+			}
+		});
+		
+		$(".blog-section__blogpost-carousel").owlCarousel({
+			loop: false,
+			items: 3,
+			dots: true,
+			autoplay: true,
+			autoplayHoverPause: true,
+			center: false,
+			autoWidth: false,
+			dotsContainer: ".blogpost-carousel_owl-buttons",
+			responsive: {
+				0: {
+					items: 1,
+					loop: false
+				},
+				851: {
+					items: 2
+				},
+				1151: {
+					items: 3
+				}
+			}
+		});
 
-      // Store hash
-      var hash = this.hash;
+		$("a").on('click', function(event) {
+			// Make sure this.hash has a value before overriding default behavior
+			if (this.hash !== "") {
+				// Prevent default anchor click behavior
+				event.preventDefault();
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-	});
+				// Store hash
+				var hash = this.hash;
+
+				// Using jQuery's animate() method to add smooth page scroll
+				// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top
+				}, 800, function(){
+		
+					// Add hash (#) to URL when done scrolling (default click behavior)
+					window.location.hash = hash;
+				});
+			} // End if
+		});
 	
 });
